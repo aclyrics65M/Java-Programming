cd58825789506e26e40d9273b57087edb9ff9002
Faraz Mamaghani <fgmamagh315@gmail.com>
2019-03-13 22:26:56 -0400

    Create WelcomeThreeMessages.java

---
+++ Intro to Java Programming Daniel Liang/Chapter 1/WelcomeThreeMessages.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class WelcomeThreeMessages {
+
+	public static void main(String[] args) {
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+
+}
