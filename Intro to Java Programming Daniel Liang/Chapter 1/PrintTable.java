44b44e5202aa6571174e74ece2083ecea1175901
Faraz Mamaghani <fgmamagh315@gmail.com>
2019-03-13 22:27:27 -0400

    Create PrintTable.java

---
+++ Intro to Java Programming Daniel Liang/Chapter 1/PrintTable.java
@@ -0,0 +1,16 @@
+package chapter1;
+
+public class PrintTable {
+
+	public static void main(String[] args) {
+		System.out.println("Table is printed below: ");
+		System.out.println(); // blank space
+		System.out.println("a    a^2    a^3");
+		System.out.println("1     1      1");
+		System.out.println("2     4      8");
+		System.out.println("3     9      27");
+		System.out.println("4     16     64");
+
+	}
+
+}
