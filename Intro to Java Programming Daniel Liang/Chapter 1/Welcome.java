bd69c7d333f0e3c1ae278ada911921f86706fe08
Faraz Mamaghani <fgmamagh315@gmail.com>
2019-03-13 22:26:23 -0400

    Create Welcome.java

---
+++ Intro to Java Programming Daniel Liang/Chapter 1/Welcome.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// Display message Welcome to Java! on the console
+		System.out.println("Welcome to Java!");
+		System.out.println("Hello World! Java Programming is fun to learn!");
+	}
+
+}
