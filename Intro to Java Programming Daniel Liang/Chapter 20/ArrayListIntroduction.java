2fcb24614dfd8b4d39d6dd8262047ea9d59a9701
Faraz Mamaghani <fgmamagh315@gmail.com>
2019-03-17 15:02:06 -0400

    Create ArrayListIntroduction.java

---
+++ Intro to Java Programming Daniel Liang/Chapter 20/ArrayListIntroduction.java
@@ -0,0 +1,24 @@
+package chapter20;
+
+public class ArrayListIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Chapter 20 in Daniel Liang's Java Textbook!");
+		System.out.println("In this chapter you will learn the basics of ArrayLists!");
+		System.out.println("As well as the following: Lists, Stacks, Queues, and Priority Queues!");
+		
+		System.out.println("\n"); // Two blank spaces
+		
+		System.out.println("You will master the following programming languages as well too:");
+		
+		System.out.println("1. Matlab Programming");
+		System.out.println("2. Java Programming");
+		System.out.println("3. C/C++ Programming");
+		System.out.println("4. Python Programming");
+		System.out.println("5. R Programming");
+		
+	}
+
+}
